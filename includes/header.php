ddc0af266c4b9b9afecb227471bceab5d08a43b3
Trung Vũ Thành <trungtovang999@gmail.com>
2024-11-06 11:10:47 +0700

    sửa để nhập được css

--- includes/header.php
+++ includes/header.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Website Mua Bán Sản Phẩm</title>
-    <link rel="stylesheet" href="/style.css" type="text/css"> 
+    <link rel="stylesheet" href="style.css" type="text/css"> 
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 </head>
 <body>
@@ -12,7 +12,7 @@
         <h1>MDH Mua Bán Sản Phẩm</h1>
         <nav>
             <ul>
-                <li><a href="/">Trang Chủ</a></li>
+                <li><a href="index.php">Trang Chủ</a></li>
                 <li><a href="admin/admin_dashboard.php">Quản trị</a></li>
             </ul>
         </nav>
